00ab4569bdbaca12febb830c8a2f0db6ba50b3a6
Ayrat <gpsdom@gmail.com>
2014-04-03 04:03:06 +0400

    work commit

---
+++ app-server/client-distribution/src/main/java/org/example/Activator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created by ayrat on 03.04.14.
+ */
+public class Activator implements BundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
