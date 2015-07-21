65864b7e33349d3eb2602642dc3075a6347674b4
Ayrat(home) <gpsdom@gmail.com>
2015-07-21 14:59:22 +0300

    wc

--- bio4j-ng-framework/s-api/src/main/java/ru/bio4j/ng/service/api/ConfigProvider.java
+++ bio4j-ng-framework/s-api/src/main/java/ru/bio4j/ng/service/api/ConfigProvider.java
@@ -3,7 +3,7 @@ package ru.bio4j.ng.service.api;
 /**
  * Created by ayrat on 08.05.14.
  */
-public interface ConfigProvider<T> {
+public interface ConfigProvider {
     public BioConfig getConfig();
     public boolean configIsRedy();
 }
