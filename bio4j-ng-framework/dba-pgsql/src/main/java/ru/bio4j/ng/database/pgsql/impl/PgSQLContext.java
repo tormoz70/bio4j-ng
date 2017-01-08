fdd477c8b97b648181c1efbb7a3ef154203d9fe4
Ayrat <gpsdom@gmail.com>
2017-01-08 17:21:57 +0300

    wc

--- bio4j-ng-framework/dba-pgsql/src/main/java/ru/bio4j/ng/database/pgsql/impl/PgSQLContext.java
+++ bio4j-ng-framework/dba-pgsql/src/main/java/ru/bio4j/ng/database/pgsql/impl/PgSQLContext.java
@@ -6,12 +6,32 @@ import org.slf4j.LoggerFactory;
 import ru.bio4j.ng.database.commons.*;
 
 import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
 
 public class PgSQLContext extends DbContextAbstract {
     private static final Logger LOG = LoggerFactory.getLogger(PgSQLContext.class);
 
     public PgSQLContext(DataSource cpool, SQLConnectionPoolConfig config) throws Exception {
         super(cpool, config);
+
+        if(this.config.getCurrentSchema() != null) {
+            this.innerAfterEvents.add(
+                    new SQLConnectionConnectedEvent() {
+                        @Override
+                        public void handle(SQLContext sender, Attributes attrs) throws SQLException {
+                            if(attrs.getConnection() != null) {
+                                String curSchema = sender.getConfig().getCurrentSchema().toUpperCase();
+                                LOG.debug("onAfterGetConnection - start setting current_schema="+curSchema);
+                                CallableStatement cs1 = attrs.getConnection().prepareCall( "SET search_path = "+curSchema);
+                                cs1.execute();
+                                LOG.debug("onAfterGetConnection - OK. current_schema now is "+curSchema);
+                            }
+                        }
+                    }
+            );
+        }
+
         wrappers = new PgSQLWrappersImpl(this.getDBMSName());
         DbUtils.getInstance().init(
                 new PgSQLTypeConverterImpl(),
