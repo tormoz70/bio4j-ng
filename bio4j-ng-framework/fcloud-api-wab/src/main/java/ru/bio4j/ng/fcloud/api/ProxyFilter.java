57e34c80bab78f89e222a40407d65f84480e4898
Ayrat(bingo) <gpsdom@gmail.com>
2018-05-13 02:00:14 +0300

    цс

--- bio4j-ng-framework/fcloud-api-wab/src/main/java/ru/bio4j/ng/fcloud/api/ProxyFilter.java
+++ bio4j-ng-framework/fcloud-api-wab/src/main/java/ru/bio4j/ng/fcloud/api/ProxyFilter.java
@@ -1,5 +1,7 @@
 package ru.bio4j.ng.fcloud.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.bio4j.ng.service.types.BioWrappedRequest;
 import ru.bio4j.ng.service.types.WarSecurityFilterBase;
 
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ProxyFilter extends WarSecurityFilterBase implements Filter {
+    private static final Logger LOG = LoggerFactory.getLogger(ProxyFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -22,9 +25,18 @@ public class ProxyFilter extends WarSecurityFilterBase implements Filter {
             BioWrappedRequest rereq = new BioWrappedRequest(req);
             rereq.putHeader("Access-Control-Allow-Origin", "*");
             ((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Credentials", "true");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, Authorization");
             super.doFilter(rereq, response, chain);
         } catch (Exception ex) {
-            throw new ServletException(ex);
+//            throw new ServletException(ex);
+            LOG.error(null, ex);
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Credentials", "true");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, Authorization");
+            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 
--- bio4j-ng-framework/remote-api-wab/src/main/java/ru/bio4j/ng/remote/api/ProxyFilter.java
+++ bio4j-ng-framework/remote-api-wab/src/main/java/ru/bio4j/ng/remote/api/ProxyFilter.java
@@ -1,6 +1,9 @@
 package ru.bio4j.ng.remote.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.bio4j.ng.service.types.BioWrappedRequest;
+import ru.bio4j.ng.service.types.CursorParser;
 import ru.bio4j.ng.service.types.WarSecurityFilterBase;
 
 import javax.servlet.*;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ProxyFilter extends WarSecurityFilterBase implements Filter {
+    private static final Logger LOG = LoggerFactory.getLogger(ProxyFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -22,9 +26,18 @@ public class ProxyFilter extends WarSecurityFilterBase implements Filter {
             BioWrappedRequest rereq = new BioWrappedRequest(req);
             rereq.putHeader("Access-Control-Allow-Origin", "*");
             ((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Credentials", "true");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, Authorization");
             super.doFilter(rereq, response, chain);
         } catch (Exception ex) {
-            throw new ServletException(ex);
+            //throw new ServletException(ex);
+            LOG.error(null, ex);
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Credentials", "true");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, Authorization");
+            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 
