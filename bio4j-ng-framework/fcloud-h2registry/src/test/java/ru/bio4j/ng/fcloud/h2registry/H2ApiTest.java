55c718a739fe36974638e1c1f71ffdc80dd41ae4
Ayrat(FK) <gpsdom@gmail.com>
2018-05-29 18:53:25 +0300

    wc

---
+++ bio4j-ng-framework/fcloud-h2registry/src/test/java/ru/bio4j/ng/fcloud/h2registry/H2ApiTest.java
@@ -0,0 +1,27 @@
+package ru.bio4j.ng.fcloud.h2registry;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.testng.annotations.Test;
+import ru.bio4j.ng.database.api.SQLNamedParametersStatement;
+import ru.bio4j.ng.database.api.SQLParamGetter;
+import ru.bio4j.ng.database.api.SQLParamSetter;
+import ru.bio4j.ng.database.commons.DbCallableParamGetter;
+import ru.bio4j.ng.database.commons.DbCallableParamSetter;
+import ru.bio4j.ng.database.commons.DbNamedParametersStatement;
+import ru.bio4j.ng.database.commons.DbSelectableParamSetter;
+import ru.bio4j.ng.model.transport.Param;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.List;
+
+public class H2ApiTest {
+
+    @Test
+    public void getConnectionTest() throws Exception {
+        Connection conn = H2Api.getConnection("jdbc:h2:˜/test", "sa", "sa");
+        FCloudDBApi.initDB(conn);
+
+    }
+
+}
