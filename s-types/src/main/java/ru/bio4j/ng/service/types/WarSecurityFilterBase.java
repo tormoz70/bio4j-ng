b66465d880a439d346981ecbcd3a6863231a38b8
bio <gpsdom@gmail.com>
2019-05-06 11:39:34 +0300

    wc

--- s-types/src/main/java/ru/bio4j/ng/service/types/WarSecurityFilterBase.java
+++ s-types/src/main/java/ru/bio4j/ng/service/types/WarSecurityFilterBase.java
@@ -116,7 +116,8 @@ public class WarSecurityFilterBase {
             ((HttpServletResponse) response).setHeader("Access-Control-Allow-Origin", "*");
             ((HttpServletResponse) response).setHeader("Access-Control-Allow-Credentials", "true");
             ((HttpServletResponse) response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-            ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, Authorization");
+            ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, X-Pagination-Current-Page, X-Pagination-Per-Page, Authorization");
+            ((HttpServletResponse) response).setHeader("Access-Control-Expose-Headers", "Content-Disposition, X-Suggested-Filename");
             doSequrityFilter(request, response, chain);
         } catch (Exception ex) {
             LOG.error(null, ex);
@@ -124,6 +125,7 @@ public class WarSecurityFilterBase {
             ((HttpServletResponse) response).setHeader("Access-Control-Allow-Credentials", "true");
             ((HttpServletResponse) response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
             ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, X-Pagination-Current-Page, X-Pagination-Per-Page, Authorization");
+            ((HttpServletResponse) response).setHeader("Access-Control-Expose-Headers", "Content-Disposition, X-Suggested-Filename");
             ((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
