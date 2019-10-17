ff1684da08cafdc3960fcfe672aae5b90fb1bb8e
bio <gpsdom@gmail.com>
2019-10-17 16:51:54 +0300

    wc

--- t-model/src/main/java/ru/bio4j/ng/model/transport/BioError.java
+++ t-model/src/main/java/ru/bio4j/ng/model/transport/BioError.java
@@ -51,27 +51,6 @@ public class BioError extends RuntimeException {
 
     //********************************************************************************
 
-//    public static abstract class SysError extends BioError {
-//        public SysError() {
-//            super(6500);
-//        }
-//        public SysError(int code) {
-//            super(code);
-//        }
-//        public SysError(String message) {
-//            super(6500, message);
-//        }
-//    }
-//
-//    public static abstract class AppError extends BioError {
-//        public AppError() {
-//            super(6200);
-//        }
-//        public AppError(String message) {
-//            super(6200, message);
-//        }
-//    }
-
     public static class BadRequestType extends BioError {
 
         public BadRequestType() {
@@ -91,16 +70,6 @@ public class BioError extends RuntimeException {
         }
     }
 
-//    public static class LocationFail extends SysError {
-//        public LocationFail() {
-//            super();
-//        }
-//        public LocationFail(Object locationId) {
-//            super(String.format("Cursor fail location to [%s] record by pk!!!", locationId));
-//        }
-//    }
-//
-//
     public static abstract class Login extends BioError {
         public Login(int code) {
             super(code);
@@ -120,30 +89,6 @@ public class BioError extends RuntimeException {
                 super(6403, "Доступ запрещен!");
             }
         }
-
-//        public static class LoginExpired extends BioError.Login {
-//            public LoginExpired() {
-//                super(6402, "Сеанс связи не существует!");
-//            }
-//        }
-
-//        public static class UserBlocked extends BioError.Login {
-//            public UserBlocked() {
-//                super(6403, "Пользователь заблокирован!");
-//            }
-//        }
-//
-//        public static class UserNotConfirmed extends BioError.Login {
-//            public UserNotConfirmed() {
-//                super(6404, "Пользователь не подтвержден!");
-//            }
-//        }
-//
-//        public static class UserDeleted extends BioError.Login {
-//            public UserDeleted() {
-//                super(6405, "Пользователь удален!");
-//            }
-//        }
 
     }
 
